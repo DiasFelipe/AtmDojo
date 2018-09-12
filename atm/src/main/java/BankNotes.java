03cdbb06e9e4fefc4eb5be57cd0c28d7a18e0f16
Felipe Dias <fdczzz@hotmail.com>
2018-09-12 04:05:02 -0300

    Code and tests refactor

--- atm/src/main/java/BankNotes.java
+++ atm/src/main/java/BankNotes.java
@@ -1,93 +1,54 @@
+import java.util.*;
+
 public class BankNotes {
-    public int banknotesHundred; //100
-    public int banknotesFifty; //50
-    public int banknotesTwenty; //20
-    public int banknotesTen; //10
+    public List<Integer> possibleValues;
+    public Map<Integer, Integer> countBanknotes;
+
+    public List<Integer> getPossibleValues() {
+        return possibleValues;
+    }
+
+    public void setPossibleValues(List<Integer> possibleValues) {
+        this.possibleValues = possibleValues;
+    }
+
+    public Map<Integer, Integer> getCountBanknotes() {
+        return countBanknotes;
+    }
+
+    public int getCountBanknotes(Integer key) {
+        return countBanknotes.get(key);
+    }
+
+    public void setCountBanknotes(Map<Integer, Integer> countBanknotes) {
+        this.countBanknotes = countBanknotes;
+    }
 
     public BankNotes() {
-        this.banknotesHundred = 0;
-        this.banknotesFifty = 0;
-        this.banknotesTwenty = 0;
-        this.banknotesTen = 0;
+        this.possibleValues = new ArrayList<Integer>(Arrays.asList(100,50,20,10));
     }
 
-    public BankNotes(int banknotesHundred, int banknotesFifty, int banknotesTwenty, int banknotesTen) {
-        this.banknotesHundred = banknotesHundred;
-        this.banknotesFifty = banknotesFifty;
-        this.banknotesTwenty = banknotesTwenty;
-        this.banknotesTen = banknotesTen;
-    }
-
-    public int getBanknotesHundred() {
-        return banknotesHundred;
-    }
-
-    public void setBanknotesHundred(int banknotesHundred) {
-        this.banknotesHundred = banknotesHundred;
-    }
-
-    public void incrementBanknotesHundred(int increment){
-        this.banknotesHundred += increment;
-    }
-
-    public int getBanknotesFifty() {
-        return banknotesFifty;
-    }
-
-    public void setBanknotesFifty(int banknotesFifty) {
-        this.banknotesFifty = banknotesFifty;
-    }
-
-    public void incrementBanknotesFifty(int increment){
-        this.banknotesFifty += increment;
-    }
-
-    public int getBanknotesTwenty() {
-        return banknotesTwenty;
-    }
-
-    public void setBanknotesTwenty(int banknotesTwenty) {
-        this.banknotesTwenty = banknotesTwenty;
-    }
-
-    public void incrementBanknotesTwenty(int increment){
-        this.banknotesTwenty += increment;
-    }
-
-    public int getBanknotesTen() {
-        return banknotesTen;
-    }
-
-    public void setBanknotesTen(int banknotesTen) {
-        this.banknotesTen = banknotesTen;
-    }
-
-    public void incrementBanknotesTen(int increment){
-        this.banknotesTen += increment;
+    public BankNotes(List<Integer> possibleValues) {
+        this.possibleValues = possibleValues;
     }
 
     public void countBankNotes(int totalValue) {
-        if(totalValue>=100) {
-            incrementBanknotesHundred(1);
-            countBankNotes(totalValue-100);
-        } else if(totalValue>=50) {
-            incrementBanknotesFifty(1);
-            countBankNotes(totalValue-50);
-        } else if(totalValue>=20) {
-            incrementBanknotesTwenty(1);
-            countBankNotes(totalValue-20);
-        } else if(totalValue>=10) {
-            incrementBanknotesTen(1);
-            countBankNotes(totalValue-10);
-        } else if(totalValue!=0) {
+        Map<Integer,Integer> banknotes = new HashMap<>();
+        for(int banknote : possibleValues) {
+            banknotes.put(banknote, totalValue/banknote);
+            totalValue = totalValue%banknote;
+        }
+        if(totalValue!=0) {
             throw new IllegalArgumentException("Valor inválido para saque");
         }
+        setCountBanknotes(banknotes);
     }
 
     public String printResult() {
-        return "Notas de 100: "+getBanknotesHundred()+"\n"+
-                "Notas de 50: "+getBanknotesFifty()+"\n"+
-                "Notas de 20: "+getBanknotesTwenty()+"\n"+
-                "Notas de 10: "+getBanknotesTen()+"\n";
+        String response = "";
+        for(Map.Entry<Integer, Integer> entry : getCountBanknotes().entrySet()) {
+            response += "Notas de "+entry.getKey()+": "+entry.getValue()+"\n";
+        }
+        return response;
     }
 }
--- atm/src/test/java/atmTests.java
+++ atm/src/test/java/atmTests.java
@@ -7,27 +7,27 @@ public class atmTests {
     public void thirtyCountShouldPass() {
         BankNotes bankNotes = new BankNotes();
         bankNotes.countBankNotes(30);
-        assertEquals(bankNotes.getBanknotesTen(), 1);
-        assertEquals(bankNotes.getBanknotesTwenty(), 1);
+        assertEquals(bankNotes.getCountBanknotes(10), 1);
+        assertEquals(bankNotes.getCountBanknotes(20), 1);
     }
 
     @Test
     public void eightyCountShouldPass() {
         BankNotes bankNotes = new BankNotes();
         bankNotes.countBankNotes(80);
-        assertEquals(bankNotes.getBanknotesTen(), 1);
-        assertEquals(bankNotes.getBanknotesTwenty(), 1);
-        assertEquals(bankNotes.getBanknotesFifty(), 1);
+        assertEquals(bankNotes.getCountBanknotes(10), 1);
+        assertEquals(bankNotes.getCountBanknotes(20), 1);
+        assertEquals(bankNotes.getCountBanknotes(50), 1);
     }
 
     @Test
     public void oneHundredAndEightyCountShouldPass() {
         BankNotes bankNotes = new BankNotes();
         bankNotes.countBankNotes(180);
-        assertEquals(bankNotes.getBanknotesTen(), 1);
-        assertEquals(bankNotes.getBanknotesTwenty(), 1);
-        assertEquals(bankNotes.getBanknotesFifty(), 1);
-        assertEquals(bankNotes.getBanknotesHundred(), 1);
+        assertEquals(bankNotes.getCountBanknotes(10), 1);
+        assertEquals(bankNotes.getCountBanknotes(20), 1);
+        assertEquals(bankNotes.getCountBanknotes(50), 1);
+        assertEquals(bankNotes.getCountBanknotes(100), 1);
     }
 
     @Test(expected = IllegalArgumentException.class)
